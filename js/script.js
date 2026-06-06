fc7ba3ec1de1fd5c2f4cb27e3ea9fd18d78ef684
andrewojak1618-debug <andrewojak1618@gmail.com>
2026-06-07 01:17:20 +0200

    added delete button and product actions button

---
+++ js/script.js
@@ -0,0 +1,15 @@
+﻿function changeQuantity(quantityId, changeValue) {
+  let quantityRef = document.getElementById(quantityId);
+  let currentQuantity = parseInt(quantityRef.innerText);
+  let newQuantity = currentQuantity + changeValue;
+
+  if (newQuantity < 1) {
+    newQuantity = 1;
+  }
+
+  if (newQuantity > 5) {
+    newQuantity = 5;
+  }
+
+  quantityRef.innerText = newQuantity + 'x';
+}
