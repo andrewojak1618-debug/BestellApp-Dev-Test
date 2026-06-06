32c44c1f5381c8d9e6cf30700584ec2d60a220bb
andrewojak1618-debug <andrewojak1618@gmail.com>
2026-06-06 18:58:33 +0200

    start the orderapp

---
+++ js/db.js
@@ -0,0 +1,32 @@
+﻿let myDishes = [
+  {
+    name: 'Neuralink Gehirnchip (BCI)',
+    price: 12999.99,
+    description:
+      'Ein Chip, der deine kognitiven Faehigkeiten um das Dreifache verbessert.',
+  },
+  {
+    name: 'Cyberarm Titan-X',
+    price: 8499.99,
+    description:
+      'Ein humanoider Roboterarm mit verstaerkter Griffkraft und praeziser Motorik.',
+  },
+  {
+    name: 'Optische Retina MK-7',
+    price: 5799.99,
+    description:
+      'Ein kuenstliches Auge mit Nachtsicht, Zoomfunktion und digitaler Analyseanzeige.',
+  },
+  {
+    name: 'Neon Spine Booster',
+    price: 9999.99,
+    description:
+      'Eine kybernetische Wirbelsaeulen-Erweiterung fuer bessere Reflexe und Stabilitaet.',
+  },
+  {
+    name: 'Synth-Lunge V2',
+    price: 4499.99,
+    description:
+      'Eine synthetische Lunge mit verbessertem Sauerstofffilter fuer toxische Stadtluft.',
+  },
+];
