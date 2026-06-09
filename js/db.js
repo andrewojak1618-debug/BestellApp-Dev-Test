a36dd596ea796274289680abaf4b910e314229bb
andrewojak1618-debug <andrewojak1618@gmail.com>
2026-06-09 13:28:33 +0200

    added new db.js data and images for Enhancements

--- js/db.js
+++ js/db.js
@@ -28,4 +28,46 @@
     quantity: 1,
   },
 ];
+let enhancementDishes = [
+  {
+    name: 'Genboost Serum',
+    description: 'genetischer Leistungsbooster für schnellere Regeneration',
+    price: 8900,
+    image: './assets/images/genboostClean.png',
+    infoImage: './assets/images/genboostInfo.png',
+    quantity: 1,
+  },
+  {
+    name: 'Nanoheart Core',
+    description: 'synthetischer Herzverstärker für höhere Belastbarkeit',
+    price: 21500,
+    image: './assets/images/nanoheartClean.png',
+    infoImage: './assets/images/nanoheartInfo.png',
+    quantity: 1,
+  },
+  {
+    name: 'Neurolink Modul',
+    description: 'neuronale Schnittstelle für direkte Systemverbindung',
+    price: 17900,
+    image: './assets/images/neurolinkClean.png',
+    infoImage: './assets/images/neurolinkInfo.png',
+    quantity: 1,
+  },
+  {
+    name: 'Titanskin Layer',
+    description: 'adaptive Hautschicht mit erhöhter Widerstandskraft',
+    price: 14200,
+    image: './assets/images/titanskinClean.png',
+    infoImage: './assets/images/titanskinInfo.png',
+    quantity: 1,
+  },
+  {
+    name: 'Quantum Reflex',
+    description: 'Reflexverstärker für präzisere Reaktionen in Echtzeit',
+    price: 19600,
+    image: './assets/images/quantumreflexClean.png',
+    infoImage: './assets/images/quantumreflexInfo.png',
+    quantity: 1,
+  },
+];
 
